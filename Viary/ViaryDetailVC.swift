99e77c8c05b45117b4ca0a4af52c5493303b83fd
Michael Wijaya <michaelwijaya.mw20@gmail.com>
2022-01-19 20:17:10 +0300

    commit another one

---
+++ Viary/ViaryDetailVC.swift
@@ -0,0 +1,85 @@
+import UIKit
+import CoreData
+class ViaryDetailVC: UIViewController {
+
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var descriptionTV: UITextView!
+    
+    var selectedViary: Note? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if(selectedViary != nil){
+            titleTF.text = selectedViary?.title
+            descriptionTV.text = selectedViary?.desc
+        }
+    }
+
+    @IBAction func saveAction(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        let currDate = Date()
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        
+        
+        
+        if(selectedViary == nil){
+            let entity = NSEntityDescription.entity(forEntityName: "Note", in: context)
+            let newViary = Note(entity: entity!, insertInto: context)
+            newViary.id = viaryList.count as NSNumber
+            newViary.title = titleTF.text
+            newViary.desc = descriptionTV.text
+            newViary.dateAdded = formatter.string(from: currDate)
+            print(newViary.dateAdded)
+            do{
+                try context.save()
+                viaryList.append(newViary)
+                navigationController?.popViewController(animated: true)
+            }
+            catch{
+                print("Context Save Error")
+            }
+        }else{
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Note")
+            do {
+                let results: NSArray = try context.fetch(request) as NSArray
+                for result in results {
+                    let viary = result as! Note
+                    if(viary == selectedViary){
+                        viary.title = titleTF.text
+                        viary.desc = descriptionTV.text
+                        try context.save()
+                        navigationController?.popViewController(animated: true)
+                    }
+                }
+            } catch {
+                print ("Fetch Failed")
+            }
+        }
+        
+    }
+    @IBAction func DeleteViary(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Note")
+        do {
+            let results: NSArray = try context.fetch(request) as NSArray
+            for result in results {
+                let viary = result as! Note
+                if(viary == selectedViary){
+                    viary.deletedDate = Date()
+                    try context.save()
+                    navigationController?.popViewController(animated: true)
+                }
+            }
+        } catch {
+            print ("Fetch Failed")
+        }
+    }
+    
+    
+}
+
